2fb8642f23b257099de5c12d5b8eb574f9a6f340
Adam Gibson <adam@skymind.io>
2015-05-10 07:53:36 -0700

    initial commit

---
+++ src/main/java/org/nd4j/App.java
@@ -0,0 +1,13 @@
+package org.nd4j;
+
+import jcublas.JCublas2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        JCublas2.setExceptionsEnabled(true);
+    }
+}
