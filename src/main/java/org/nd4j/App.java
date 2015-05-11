bf600ddae629fb9a71453bbf295773eeabe7d97d
Adam Gibson <adam@skymind.io>
2015-05-11 13:57:33 -0700

    still broken

--- src/main/java/org/nd4j/App.java
+++ src/main/java/org/nd4j/App.java
@@ -1,6 +1,12 @@
 package org.nd4j;
 
 import jcublas.JCublas2;
+import jcublas.cublasHandle;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Hello world!
@@ -9,5 +15,12 @@ import jcublas.JCublas2;
 public class App {
     public static void main( String[] args ) {
         JCublas2.setExceptionsEnabled(true);
+        INDArray ret = Nd4j.create(5);
+       //This will break if you run it with jcublas2.
+       // ret.mmul(ret.transpose());
+
+        cublasHandle handle = new cublasHandle();
+        JCublas2.cublasCreate(handle);
+        JCublas2.cublasDestroy(handle);
     }
 }
